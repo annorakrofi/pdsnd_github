135bda6a6bead790176eb5c91fd29122f307cc74
annor akrofi <akrofiannor1@gmail.com>
2023-11-15 14:59:26 +0000

    refactor: Enhance code efficiency through optimizations.

--- bikeshare.py
+++ bikeshare.py
@@ -197,7 +197,7 @@ def main():
         trip_duration_stats(df)
         user_stats(df,city)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nAre you interested in initiating a restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
 
